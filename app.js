bbe226c183cfd7ba9d87097cdf1f427cb0ce3067
John McMenemy <j_a_mcmenemy@yahoo.com>
2020-06-04 15:30:12 +0100

    Changed Font Awesome to npm version (template was using 2013 version) hoping this will allow the icons to display on Heroku

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Serve Public directory
 app.use(express.static(path.join(__dirname, 'public')));
+// Serve FontAwesome
+app.use('/stylesheets/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
 
 app.use('/', indexRouter);
 app.use('/project', projectRouter)
