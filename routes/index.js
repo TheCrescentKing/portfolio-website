7ed5277c7229c7103f75d651c5b40caaf793dbec
John McMenemy <j_a_mcmenemy@yahoo.com>
2020-06-04 12:11:06 +0100

    Decided to redisign the whole page using a template and looks much better now.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var dataFunctions = require('../utils-module').projectDataUtils;
 var router = express.Router();
 
+
+let aboutInfo = "Hi, I’m John. A trilingual, lifelong learner who loves technology, science, psychology, philosophy, exercise and nutrition. I feel at my best when I’m doing something that I know is helping people with their lives.";
+
 // I don't think this is ideal, does this make the whole website not render unltil the projects are returned?
 // But maybe I can call the async function in the pug template?
 /* GET home page. */
@@ -12,7 +15,7 @@ router.get('/', function(req, res, next) {
       projects = ['An error has occurred.']
     }
 
-    res.render('index', { title: 'JAMM\'s Portfolio', projects:  projects});
+    res.render('index', { title: 'JAMM\'s Portfolio', projects:  projects, aboutInfo: aboutInfo});
   });
 });
 
