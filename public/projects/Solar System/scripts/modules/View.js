e019ce59bc2ed6277b1a2b30f5b5c7c7b5b01238
John McMenemy <j_a_mcmenemy@yahoo.com>
2020-06-07 14:55:36 +0100

    Added my C++ Project to the projects page, and added the feature to embed youtube videos instead of using a preview image

--- public/projects/Solar System/scripts/modules/View.js
+++ public/projects/Solar System/scripts/modules/View.js
@@ -2,5 +2,5 @@ const canvas = document.querySelector('canvas');
 
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
